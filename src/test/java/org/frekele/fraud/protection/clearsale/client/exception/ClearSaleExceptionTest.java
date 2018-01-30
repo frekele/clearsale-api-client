98c444ed1d0521a6037358b0e56614e93a6d042e
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-01-30 03:48:41 -0200

    ClearSaleExceptionTest

---
+++ src/test/java/org/frekele/fraud/protection/clearsale/client/exception/ClearSaleExceptionTest.java
@@ -0,0 +1,35 @@
+package org.frekele.fraud.protection.clearsale.client.exception;
+
+import org.frekele.fraud.protection.clearsale.client.testng.InvokedMethodListener;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+@Listeners(InvokedMethodListener.class)
+public class ClearSaleExceptionTest {
+
+    String msg = "Error Message";
+
+    IOException cause = new IOException("IO error");
+
+    @Test(expectedExceptions = ClearSaleException.class)
+    public void testConstructorWithMessage() throws Exception {
+        throw new ClearSaleException(msg);
+    }
+
+    @Test(expectedExceptions = ClearSaleException.class)
+    public void testConstructorWithCause() throws Exception {
+        throw new ClearSaleException(cause);
+    }
+
+    @Test(expectedExceptions = ClearSaleException.class)
+    public void testConstructorWithMessageAndCause() throws Exception {
+        throw new ClearSaleException(msg, cause);
+    }
+
+    @Test(expectedExceptions = ClearSaleException.class)
+    public void testConstructorWithMessageAndCauseAndMore() throws Exception {
+        throw new ClearSaleException(msg, cause, true, true);
+    }
+}
