26226a388562c616f256b26e8c8abcc2f68eed85
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-01-30 03:45:56 -0200

    ClearSaleEntity

---
+++ src/main/java/org/frekele/fraud/protection/clearsale/client/core/ClearSaleEntity.java
@@ -0,0 +1,10 @@
+package org.frekele.fraud.protection.clearsale.client.core;
+
+import java.io.Serializable;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+public interface ClearSaleEntity extends Serializable {
+
+}
